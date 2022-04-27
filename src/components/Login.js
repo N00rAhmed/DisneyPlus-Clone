53f168b1042e1b5958e53791dee40f8285126c90
unknown <tronn232003@gmail.com>
2022-04-27 01:36:19 +0100

    created background

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 const Login = (props) => {
     return(
         <Container>
-            <Content>Content</Content>
+            <Content>
+                <CTA>
+                    <CTALogoOne src='/images/cta-logo-one.svg' alt='' />
+                </CTA>
+                <BgImage />
+            </Content>
         </Container>
     );
 
@@ -27,6 +32,54 @@ display: flex;
 justify-content: center;
 align-items: center;
 flex-direction: column;
+padding: 80px 40px;
+height: 100%;
 
 `;
+
+const BgImage = styled.div`
+height: 100%;
+background-position: top;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url('/images/login-background.jpg');
+position: absolute;
+top: 0;
+right: 0;
+left: 0;
+z-index: -1;
+`;
+
+// const CTA = styled.div`
+// margin-bottom: 2px;
+// max=width: 650px;
+// flex-wrap: wrap;
+// display: flex;
+// flex-direction: column;
+// justify-content: centre;
+// margin-top: 0;
+// align-items: center;
+// text-align: center;
+// margin-right: auto;
+// margin-left: auto;
+// transition-timing-function: ease-out;
+// transition: opacity 0.2s;
+// width: 100%;
+// `;
+
+const CTA = styled.div`
+max-width: 650px;
+width: 100%;
+display: flex;
+flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+`; 
+
 export default Login;
