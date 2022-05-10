60e554443f8f44521f6286c63f6941d74c5d2a6f
muntasir2001 <muntasirenamalhuda@gmail.com>
2022-05-10 12:12:15 +0100

    fixed google signin modal firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@
 // import 'firebase/compat/auth';
 // import 'firebase/compat/firestore';
 
-
 // const firebaseConfig = {
 //     apiKey: "AIzaSyBKcoE32BlFeffE6sjlCgKR5v37KFVTwGg",
 //     authDomain: "disneyplus-2b68c.firebaseapp.com",
@@ -24,24 +23,23 @@
 
 // export default db;
 
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKcoE32BlFeffE6sjlCgKR5v37KFVTwGg",
-  authDomain: "disneyplus-2b68c.firebaseapp.com",
-  projectId: "disneyplus-2b68c",
-  storageBucket: "disneyplus-2b68c.appspot.com",
-  messagingSenderId: "55512103767",
-  appId: "1:55512103767:web:2b41b0fccb0d831ae71c12",
-  measurementId: "G-EG5QMXCPS5",
+	apiKey: 'AIzaSyBKcoE32BlFeffE6sjlCgKR5v37KFVTwGg',
+	authDomain: 'disneyplus-2b68c.firebaseapp.com',
+	projectId: 'disneyplus-2b68c',
+	storageBucket: 'disneyplus-2b68c.appspot.com',
+	messagingSenderId: '55512103767',
+	appId: '1:55512103767:web:2b41b0fccb0d831ae71c12',
+	measurementId: 'G-EG5QMXCPS5',
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore();
-const auth = getAuth();
+const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
 const storage = getStorage();
 
